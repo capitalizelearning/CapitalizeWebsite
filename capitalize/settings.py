5e0e52b7a476f9b7f2706d51e45ea354e942e2be
Colby <colby.b.cooper@gmail.com>
2024-03-03 21:32:03 -0500

    Update DEBUG setting and static file configuration

--- capitalize/settings.py
+++ capitalize/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['capitalizelearn.com', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
@@ -140,8 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
