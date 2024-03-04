dbc1184578af2242f653a89be884279d8e9f3e3f
Colby <colby.b.cooper@gmail.com>
2024-03-03 21:30:39 -0500

    Add CORS allowed origin for production environment

--- capitalize/settings.py
+++ capitalize/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['capitalizelearn.com', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://capitalizelearn.com",
 ]
 
 # Application definition
